eb8fe7268226673040bae8e519cedb2e1de40e6b
Johan Svensson <85johsve@skola.boras.se>
2022-10-26 12:42:55 +0200

    some connection to database

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using MySqlConnector;
+
+public class Movie
+{
+    public int MovieBarcode { get; set; }
+    public string MovieName { get; set; }
+    public int DatePublished { get; set; }
+    public double MoviePrice { get; set; }
+    public bool isOutdated { get; set; }
+    public bool isRented { get; set; }
+    
+    public override string ToString()
+    {
+        return $"{MovieName}";
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (var connection = new MySqlConnection("Server=localhost;Database=videotek;Uid=Johan;Pwd=abc123;"))
+        {
+            var movie = connection.Query<Movie>("SELECT movie_name FROM movie;").ToList();
+            foreach(var item in movie)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
